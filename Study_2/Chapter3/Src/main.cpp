b1fc6fd045832b6db2faacc55a12822c4b5bd0a7
maru1078 <g019g1420@gmail.com>
2020-06-09 17:06:10 +0900

    Chapter3開始

---
+++ Study_2/Chapter3/Src/main.cpp
@@ -0,0 +1,180 @@
+#include <Windows.h>
+#include <tchar.h>
+
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+using namespace std;
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// @brief コンソール画面にフォーマット付き文字列を表示
+// @param format フォーマット（%dとか%fとかの）
+// @param 可変長引数
+// @remarks この関数はデバッグ用です。デバッグ時にしか動作しません
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+
+#endif
+}
+
+const int window_width = 960;
+const int window_height = 540;
+
+// 面倒だけど書かなければいけない関数
+HRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0); // OSに対して「もうこのアプリは終わる」と伝える
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam); // 既定の処理を行う
+}
+
+#ifdef _DEBUG
+
+int main()
+{
+
+#else
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+
+#endif
+
+	DebugOutputFormatString("Show window test.");
+
+	// ウィンドウクラスの生成＆登録
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure; // コールバック関数の指定
+	w.lpszClassName = _T("DX12Sample");       // アプリケーションクラス名（適当でよい）
+	w.hInstance = GetModuleHandle(nullptr);   // ハンドルの取得
+
+	RegisterClassEx(&w); // アプリケーションクラス（ウィンドウクラスの指定をOSに伝える）
+
+	RECT wrc = { 0, 0, window_width, window_height }; // ウィンドウサイズを決める
+
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(
+		w.lpszClassName,      // クラス名指定
+		_T("DX12テスト"),	  // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,  // タイトルバーと境界があるウィンドウ
+		CW_USEDEFAULT,		  // 表示 X 座標はOSにお任せ
+		CW_USEDEFAULT,		  // 表示 Y 座標はOSにお任せ
+		wrc.right - wrc.left, // ウィンドウ幅
+		wrc.bottom - wrc.top, // ウィンドウ高
+		nullptr,			  // 親ウィンドウハンドル
+		nullptr,			  // メニューハンドル
+		w.hInstance,		  // 呼び出しアプリケーションハンドル
+		nullptr);			  // 追加パラメーター
+
+	// ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	// ==========DirectX12関係==========
+
+	ComPtr<ID3D12Device>    _dev{ nullptr };
+	ComPtr<IDXGIFactory6>   _dxgiFactory{ nullptr };
+	ComPtr<IDXGISwapChain4> _swapChain{ nullptr };
+
+	// Direct3Dデバイスの初期化
+	D3D_FEATURE_LEVEL levels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+	
+	D3D_FEATURE_LEVEL featureLevel;
+	
+	for (auto lv : levels)
+	{
+		if (D3D12CreateDevice(nullptr, lv, IID_PPV_ARGS(_dev.ReleaseAndGetAddressOf())))
+		{
+			featureLevel = lv;
+			break; // 生成可能なバージョンが見つかったらループを打ち切り
+		}
+	}
+
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(_dxgiFactory.ReleaseAndGetAddressOf()));
+
+	ComPtr<ID3D12CommandAllocator>    _cmdAllocator{ nullptr };
+	ComPtr<ID3D12GraphicsCommandList> _cmdList{ nullptr };
+	ComPtr<ID3D12CommandQueue>        _cmdQueue{ nullptr };
+
+	// コマンドアロケーター作成
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(_cmdAllocator.ReleaseAndGetAddressOf()));
+
+	// コマンドリスト作成
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator.Get(), nullptr, IID_PPV_ARGS(_cmdList.ReleaseAndGetAddressOf()));
+
+	// コマンドキュー作成
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc{};
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE; // フラグなし
+	cmdQueueDesc.NodeMask = 0; // アダプターを1つしか使わないときは0でよい
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL; // プライオリティは特に指定なし
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT; // コマンドリストと合わせる
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(_cmdQueue.ReleaseAndGetAddressOf()));
+
+	// スワップチェーン作成
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = window_width;
+	swapChainDesc.Height = window_height;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.Stereo = false;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.Scaling = DXGI_SCALING_STRETCH; // バックバッファーは伸び縮み可能
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // フリップ後は速やかに破棄
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED; // 特に指定なし
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // ウィンドウ⇔フルスクリーン切り替え可能
+	result = _dxgiFactory->CreateSwapChainForHwnd(_cmdQueue.Get(), hwnd, &swapChainDesc, nullptr, nullptr, (IDXGISwapChain1**)_swapChain.ReleaseAndGetAddressOf());
+
+	// ループ
+	MSG msg{};
+	while (true)
+	{
+		if (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// アプリケーションが終わるときに message が WM_QUIT になる
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	// もうクラスは使わないので登録解除する
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
+
